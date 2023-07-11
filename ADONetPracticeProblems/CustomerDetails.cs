944084508276713851e7e11fcb910967706a4150
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-11 20:38:24 +0530

    [somu]Add:Creating DataBase Using Ado.net in FrameWork.

---
+++ ADONetPracticeProblems/CustomerDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONetPracticeProblems
+{
+    public class CustomerDetails
+    {
+        public static void CreateDataBase()
+        {
+            SqlConnection connection = new SqlConnection("data source=(localdb)\\MSSQLLocalDB; initial catalog=master; integrated security=true");
+            try
+            {
+                string query = "Create Database ADONetPracticeProblems";
+                SqlCommand command = new SqlCommand(query,connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("DataBase Created Successfully");
+                Console.WriteLine("------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something Went Wrong"+ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ ADONetPracticeProblems/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ADONetPracticeProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Option to Perform Operation\n1.Create DataBase\n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        CustomerDetails.CreateDataBase();
+                        break;
+                        case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
